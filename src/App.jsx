4011e194cd99658eb56c2460a12d03aee0fff571
cactus <69766049+c4ctus96@users.noreply.github.com>
2025-12-24 22:58:45 +0100

    router optimized for gh pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,25 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useEffect } from "react";
+import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom";
 import MainPage from './components/MainPage'
 import AdminPanel from './components/AdminPanel'
 import UploadGpx from './components/UploadGpx'
 
 function App() {
+  const navigate = useNavigate();
+  useEffect(() => {
+    const params = new URLSearchParams(window.location.search);
+    const redirect = params.get("redirect");
+    if (redirect) {
+      navigate(redirect, { replace: true });
+    }
+  }, [navigate]);
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<MainPage />} />
-        <Route path="/admin" element={<AdminPanel />} />
-        <Route path="/admin/uploadGpx" element={<UploadGpx />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<MainPage />} />
+      <Route path="/admin" element={<AdminPanel />} />
+      <Route path="/admin/uploadGpx" element={<UploadGpx />} />
+    </Routes>
   )
 }
 
