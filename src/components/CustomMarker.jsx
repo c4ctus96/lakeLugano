4b61f13798aeeccfbfee0e837bfee92c5f1ba25b
cactus <69766049+c4ctus96@users.noreply.github.com>
2026-02-21 00:31:23 +0100

    mode and type dependent marker icons for trails

--- src/components/CustomMarker.jsx
+++ src/components/CustomMarker.jsx
@@ -2,13 +2,32 @@ import React, { useEffect, useState } from "react";
 import ReactDOMServer from 'react-dom/server';
 import L from "leaflet";
 import { Marker } from 'react-leaflet';
+
 import { PiTrain } from "react-icons/pi";
+import { BiWalk } from "react-icons/bi";
+import { MdOutlineDirectionsBike } from "react-icons/md";
+import { MdOutlineElectricBike } from "react-icons/md";
+import { GrAnchor } from "react-icons/gr";
+import { FaCircleDot } from "react-icons/fa6";
+
+
 
 export default function CustomMarker({ position, type, onClick }) {
+    const iconSize = 28;
+
+    const icons = {
+        "hikeTrail": <BiWalk size={iconSize} className="icon" />,
+        "bikeTrail": <MdOutlineDirectionsBike size={iconSize} className="icon" />,
+        "eBikeStation": <MdOutlineElectricBike size={iconSize} className="icon" />,
+        "boatStation": <GrAnchor size={iconSize} className="icon" />,
+        "trainStation": <PiTrain size={iconSize} className="icon" />,
+        "default": <FaCircleDot size={iconSize} className="icon" />
+    }
+
     const icon = L.divIcon({
         html: ReactDOMServer.renderToString(
             <div className="marker">
-                <PiTrain size={30} className="icon" />
+                {icons[type] || icons["default"]}
             </div>
         ),
         iconSize: [40, 40],
--- src/components/TrailsOverlay.jsx
+++ src/components/TrailsOverlay.jsx
@@ -3,33 +3,55 @@ import { Polyline } from 'react-leaflet'
 import { useFocus } from '../context/FocusContext';
 import CustomMarker from "./CustomMarker";
 import { useTrails } from '../context/TrailsContext';
+import { useMode } from '../context/ModeContext';
 
 function TrailsOverlay() {
     const { setFocus } = useFocus();
     const { trails } = useTrails();
+    const { mode } = useMode();
 
     const difficultyColors = [
         "green", "yellow", "red", "black"
     ]
-    
+
+    const preferredTypes = {
+        "walk": ["hikeTrail"],
+        "bike": ["bikeTrail", "eBikeStation"],
+        "explore": [],
+        "commute": [],
+        "boats": ["boatStation"]
+    }
+
     console.log("TrailsOverlay trails:", trails);
 
-    return (
-        <>
-            {trails.map((route, i) => (
-                <React.Fragment key={`group ${i}`}>
-                    <Polyline key={`route ${i}`} positions={route.track.map(point => [point.latitude, point.longitude])}
-                        color={difficultyColors[route.difficulty] || "blue"}
-                        weight={4} />
-                    <CustomMarker key={`marker ${i}`} position={[route.track[0].latitude, route.track[0].longitude]}
+
+    return trails.map((route, i) => {
+        const selectedType =
+            preferredTypes[mode].find(t => route.types.includes(t)) ||
+            route.types[0];
+            console.log(`Route ${i} selected type:`, selectedType);
+
+        return (
+            <React.Fragment key={`group ${i}`}>
+                <Polyline
+                    key={`route ${i}`}
+                    positions={route.track.map(point => [point.latitude, point.longitude])}
+                    color={difficultyColors[route.difficulty] || "blue"}
+                    weight={4}
+                />
+
+                <CustomMarker
+                    key={`marker ${i}`}
+                    position={[route.track[0].latitude, route.track[0].longitude]}
+                    type={selectedType}
                     onClick={() => {
                         setFocus(route.id);
                         console.log(`Marker ${i} clicked`);
-                    }}/>
-                </React.Fragment>
-            ))}
-        </>
-    );
+                    }}
+                />
+            </React.Fragment>
+        );
+    })
 }
 
 export default TrailsOverlay;
