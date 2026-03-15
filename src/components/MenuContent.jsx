59574724e6423ad1a0be6b9176fd13cfb4bc2650
cactus <69766049+c4ctus96@users.noreply.github.com>
2026-03-15 23:00:19 +0100

    menu tags added

--- src/components/MenuContent.jsx
+++ src/components/MenuContent.jsx
@@ -2,7 +2,15 @@ import React, { useEffect, useState } from "react";
 import { useFocus } from '../context/FocusContext.jsx';
 import { useObjects } from '../context/ObjectsContext.jsx';
 import { useTranslation } from 'react-i18next';
+
+import { difficultyColors } from "../constants/difficultyColors.js";
+
 import { IoClose } from "react-icons/io5";
+import { MdSignalCellular1Bar } from "react-icons/md";
+import { MdSignalCellular2Bar } from "react-icons/md";
+import { MdSignalCellular3Bar } from "react-icons/md";
+import { MdSignalCellular4Bar } from "react-icons/md";
+import { LuRuler } from "react-icons/lu";
 
 
 export default function MenuContent() {
@@ -11,13 +19,34 @@ export default function MenuContent() {
     const { t } = useTranslation();
 
     const selectedTrail = objects.find(coords => coords.id === focus);
-
+    console.log(selectedTrail?.difficulty);
+    console.log(difficultyColors[selectedTrail?.difficulty]);
     return (
         <div id="menuContent">
             <button onClick={() => { setFocus(null) }}><IoClose /></button>
             <h2>{selectedTrail?.name}</h2>
-            {selectedTrail?.lengthKm && <p>{t("menu.length")}: {selectedTrail?.lengthKm} km</p>}
-            {selectedTrail?.difficulty && <p>{t("menu.difficulty")}: {t(`difficulty.${selectedTrail?.difficulty}`)}</p>}
+            <div id="tags">
+                {(selectedTrail?.difficulty !== undefined) &&
+                    <div className="bubble">
+                        <span style={{
+                            height: '8px',
+                            width: '8px',
+                            backgroundColor: difficultyColors[selectedTrail?.difficulty] || 'gray',
+                            borderRadius: '50%',
+                            display: 'inline-block',
+                            marginRight: '8px'
+                        }}>
+                        </span>
+                        {t(`difficulty.${selectedTrail?.difficulty}`)}
+                    </div>
+                }
+                {selectedTrail?.lengthKm &&
+                    <div className="bubble">
+                        <LuRuler style={{ marginRight: '8px' }} />
+                        {selectedTrail.lengthKm} km
+                    </div>
+                }
+            </div>
             <p>{selectedTrail?.description}</p>
         </div>
     );
